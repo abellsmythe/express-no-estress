eb7ed2ac26ad6753cbd8643145c3f7546aaa9f19
{Alton Bell Smythe} <abellsmythe@gmail.com>
2019-04-18 11:18:37 -0300

    Improvement to handle services

--- app/services/index.js
+++ app/services/index.js
@@ -1,7 +1,19 @@
 "use strict";
 
+const services = {};
+const normalizedPath = require("path").join(__dirname, "./");
+
+require("fs").readdirSync(normalizedPath).forEach(function(file) {
+    const splitFileName = file.split('.');
+    const fileName      = splitFileName[0];
+
+    if (fileName !== 'index') {
+        services[fileName] = require(`./${fileName}`);
+    }
+});
+
 module.exports = (app) => {
-    const mongo     = require("./mongo");
-    const redis     = require("./redis");
-    const limiter   = require("./limiter")(app);
-}
+    Object.entries(services).forEach(([name, service]) => {
+        service(app);
+    });
+};
