4ab73959c5fb677e5cd8afd637f21dea2d8f1771
{Alton Bell Smythe} <abellsmythe@gmail.com>
2019-05-05 16:55:43 -0300

    Add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [
+    {
+      name: 'API',
+      script: 'app.js',
+      args: 'one two',
+      instances: 4,
+      autorestart: true,
+      watch: true,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+};
