49ccf0db69ad7c1aa5b153564b6c694ff24ef349
Alton Bell Smythe <abellsmythe@gmail.com>
2019-05-14 10:50:37 -0300

    Update Uset Event with Typescript

--- api/events/UserEvent.ts
+++ api/events/UserEvent.ts
@@ -1,17 +1,20 @@
 // Event Emitter
-const eventEmitter = require("../helpers/event-emitter");
+import eventEmitter from '../helpers/event-emitter';
 
 // Create User
 eventEmitter.on('User-Created', (data) => {
+  // tslint:disable-next-line no-console
   console.log('User Created', data);
 });
 
 // Update User
 eventEmitter.on('User-Updated', (data) => {
+  // tslint:disable-next-line no-console
   console.log('User Updated', data);
 });
 
 // Delete User
 eventEmitter.on('User-Deleted', (data) => {
+  // tslint:disable-next-line no-console
   console.log('User Deleted', data);
 });
